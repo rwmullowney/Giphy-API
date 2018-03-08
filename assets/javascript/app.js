1fec979aa64ab7ec4a91408e7b5e2d5a2304f0bb
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-03-08 12:56:39 -0600

    Adds JS functionality so clicking on button shows 5 gifs from the Giphy API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
 // Array of topics we'll use to create buttons
 var topics = ['dogs', 'cats', 'parakeets']
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YcMY50f8vA1dUzRPHD7e9oQ3eiOBNXv0&q=ryan+gosling&limit=5";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YcMY50f8vA1dUzRPHD7e9oQ3eiOBNXv0&q=cats&limit=5";
+
+$("#cat-button").on('click', function(){
+    $.get(queryURL)
+    .then(function(response){
+
+        for (var i = 0; i < 5; i++){
+        var imageURL = response.data[i].images.original.url;
+
+        var animalImage = $("<img>");
+        animalImage.attr("src", imageURL);
+        animalImage.attr("alt", 'cat image');
+
+        $("#gifs").append(animalImage)
+    }
+    })
+})
