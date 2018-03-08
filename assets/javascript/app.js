0c29cfaa33f71c2eb1d9f3243736c0580d356398
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-03-08 15:28:32 -0600

    Adds JS functionality so buttons are generated from items in an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,16 @@
 // Array of topics we'll use to create buttons
-var topics = ['dogs', 'cats', 'parakeets']
+var topics = ['Dogs', 'Cats', 'Parakeets']
+
+for (var i = 0; i < topics.length; i++){
+    var gifButton = "<button>" + topics[i] + "</button>"
+    $("#buttons").append(gifButton)
+}
 
 
 
 $("button").on('click', function(){
-    var animal = $(this).attr("data-animal")
+    var animal = $(this).text()
+    console.log(animal)
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YcMY50f8vA1dUzRPHD7e9oQ3eiOBNXv0&q=" + animal + "&limit=5";
 
     $.get(queryURL)
@@ -17,7 +23,7 @@ $("button").on('click', function(){
         animalImage.attr("src", imageURL);
         animalImage.attr("alt", 'cat image');
 
-        $("#gifs").append(animalImage)
+        $("#gifs").prepend(animalImage)
     }
     })
 })
