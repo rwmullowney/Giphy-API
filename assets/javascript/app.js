c58d6cca86b59f51bcc8163e0be2aa6c4b340167
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-03-08 12:34:29 -0600

    Adds 'sections' to HTML to make it easier to test adding things to page.  Adds 'topics' array to JS and the queryURL for querying the Giphy API with my API key

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+// Array of topics we'll use to create buttons
+var topics = ['dogs', 'cats', 'parakeets']
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=YcMY50f8vA1dUzRPHD7e9oQ3eiOBNXv0&q=ryan+gosling&limit=5";
